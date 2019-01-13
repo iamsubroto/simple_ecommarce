7ae064c97ba31e3b89303dba763b44d5dc2bb1a6
iamsubroto <smartcoder500@gmail.com>
2019-01-13 03:25:01 -0800

    Updates

---
+++ precache-manifest.c6f425ba5b5dfaf1eecfe02c498f67e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7263f08995b8512a4f6",
+    "url": "/simple_ecommarce/static/js/runtime~main.a7263f08.js"
+  },
+  {
+    "revision": "11841eabf315fb355b1f",
+    "url": "/simple_ecommarce/static/js/main.11841eab.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/simple_ecommarce/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "11841eabf315fb355b1f",
+    "url": "/simple_ecommarce/static/css/main.57fcb92b.chunk.css"
+  },
+  {
+    "revision": "c3e030a57c521f95a234b5bd5ee81a18",
+    "url": "/simple_ecommarce/index.html"
+  }
+];
